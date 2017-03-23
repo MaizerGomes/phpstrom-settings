c98c94c02024ecb2f308cea94e54f04ed01058a5
Maizer Gomes <maizer.gomes@gmail.com>
2017-03-23 12:23:42 +0200

    PS-2016.3.3 <maizerg@maizerg-Latitude-E6520 Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, Default copy.xml, Earthsong copy.icls, applicationLibraries.xml, MaizerG.xml, file.template.settings.xml, ignore.xml, find.xml, code.style.schemes.xml, Default _3_.xml, git.xml, Zen HTML.xml, Darcula copy.icls, github_settings.xml, earthsong.icls, Default.xml, Default _2_.xml, laf.xml, vue_for_idea.xml, Zen XSL.xml, editor.codeinsight.xml, editor.xml, Blade༏ext༏.blade.php, uml.xml, webServers.xml, debugger.xml, path.macros.xml, baseRefactoring.xml, ide.general.xml, other.xml, Zen CSS.xml, keymap.xml, ui.lnf.xml, MaizerG Laravel.xml, filetypes.xml, php.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, databaseDrivers.xml, TWIG File༏ext༏.html.twig, Mai.icls, PHP File Header.php, Default _1_.xml, web-browsers.xml, databaseSettings.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,7 @@
+/**
+ * Criado por Maizer Aly de O. Gomes para ${PROJECT_NAME}.
+ * Email: maizer.gomes@gmail.com / maizer.gomes@ekutivasolutions / maizer.gomes@outlook.com
+ * Usuário: ${USER}
+ * Data: ${DATE}
+ * Hora: ${TIME}
+ */
